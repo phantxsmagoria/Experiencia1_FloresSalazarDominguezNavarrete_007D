0717d4710ff80799e539a2ff1f18863d3f80468e
Nacholoz16 <ig.salazar@duocuc.cl>
2022-06-05 21:08:16 -0400

    CreacionDeTablasBasedeDatos

--- Evaluacion/settings.py
+++ Evaluacion/settings.py
@@ -76,9 +76,17 @@ WSGI_APPLICATION = 'Evaluacion.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+        'ENGINE': 'django.db.backends.oracle',
+        'NAME':'127.0.0.1:1521/xe',
+        'USER' : 'c##prueba3',
+        'PASSWORD':'1234',
+        'TEST' : {
+            'USER' : 'default_test',
+            'TBLSPACE' : 'default_test_tbls',
+            'TBLSPACE_TMP' : 'default_test_tbls_tmp',
+
+        },
+    },
 }
 
 
